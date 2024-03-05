fc5238e7070332714205c1290313eecf4f92e6e0
think-a-little <mcsemen55577719@gmail.com>
2024-03-05 13:49:28 +0700

    Move the duplicated code to a function

--- src/Order.java
+++ src/Order.java
@@ -7,45 +7,47 @@ public class Order {
     static final String headerFemale = ("Здравствуйте, дорогая ");
     static final String footer = ("С уважением, автоматизированная система мониторинга.");
 
+    static HashSet<String> unicalKeys(HashMap<String, String> one, HashMap<String, String> two) {
+        Set<String> o = one.keySet();
+        Set<String> t = two.keySet();
+        HashSet<String> unical = new HashSet<>(o);
+        unical.removeAll(t);
+        return unical;
+    }
+
     static StringBuilder changedURL(boolean isMale, String name,
                                     HashMap<String, String> today, HashMap<String, String> yesterday) {
         StringBuilder result = new StringBuilder();
         if (isMale) result.append(headerMale);
         else result.append(headerFemale);
-        result.append(name + "\n\n");
+        result.append(name).append("\n\n");
 
         result.append("За последние сутки во вверенных Вам сайтах произошли следующие изменения:\n\n");
 
         result.append("Исчезли следующие страницы:\n");
-        Set<String> y = yesterday.keySet();
-        Set<String> t = today.keySet();
-        HashSet<String> disappear = new HashSet<>(y);
-        disappear.removeAll(t);
-        for (String item : disappear) {
-            result.append(item+"\n");
+        for (String item : unicalKeys(yesterday, today)) {
+            result.append(item).append("\n");
         }
 
         result.append("Появились следующие страницы:\n");
-        HashSet<String> appear = new HashSet(t);
-        appear.removeAll(y);
-        for (String item : appear) {
-            result.append(item+"\n");
+        for (String item : unicalKeys(today, yesterday)) {
+            result.append(item).append("\n");
         }
 
         result.append("Изменились следующие страницы:\n");
         today = new HashMap<>(today);
-        today.keySet().removeAll(appear);
+        today.keySet().removeAll(unicalKeys(today, yesterday));
         yesterday = new HashMap<>(yesterday);
-        yesterday.keySet().removeAll(disappear);
+        yesterday.keySet().removeAll(unicalKeys(yesterday, today));
         for (HashMap.Entry<String, String> entry : today.entrySet()) {
             String value1 = entry.getValue();
             String value2 = yesterday.get(entry.getKey());
             if (value2 != null && !value1.equals(value2)) {
-                result.append(entry.getKey()+"\n");
+                result.append(entry.getKey()).append("\n");
             }
         }
 
-        result.append("\n"+footer);
+        result.append("\n").append(footer);
         return result;
     }
 
